9875d7e261988d387733bc8f571ce767fdda2458
SerkanSEKENDUR <serkan.sekendur@gmail.com>
2023-06-20 19:11:10 +0300

    S1

---
+++ src/test/java/runners/DBRunner.java
@@ -0,0 +1,25 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        plugin = {
+                "pretty",
+                "html:target/html-reports/cucumber.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-reports/cucumber.xml",
+                "rerun:target/failedRerun.txt",
+        },
+        features = "./src/test/resources/features/db_features",
+        glue = {"stepdefinitions"},
+        tags = "@verify_account_types_SA",
+        dryRun = false
+
+)
+public class DBRunner {
+
+}
